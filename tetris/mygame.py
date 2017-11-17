23f77013df080fdaecbc4df16a0a63a234ae8229
2D file <kim789so@naver.com>
2017-11-17 16:06:12 +0900

    resourse update

---
+++ tetris/mygame.py
@@ -0,0 +1,16 @@
+import game_framework
+from pico2d import *
+
+# fill state here
+import start_state
+import main_state
+import pause_state
+
+
+
+#open_canvas()
+game_framework.run(start_state)
+#game_framework.run(main_state)
+#game_framework.run(pause_state)
+
+#close_canvas()
