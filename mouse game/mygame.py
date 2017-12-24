8cbb3fc5752c4fc036ab920483e3cac1b48738d4
2D file <kim789so@naver.com>
2017-12-25 04:36:12 +0900

    인스톨러 추가

--- mouse game/mygame.py
+++ mouse game/mygame.py
@@ -1,4 +1,12 @@
 import game_framework
+import platform
+import os
+
+if platform.architecture()[0] == '32bit':
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86"
+else:
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
+
 from pico2d import *
 
 # fill state here
